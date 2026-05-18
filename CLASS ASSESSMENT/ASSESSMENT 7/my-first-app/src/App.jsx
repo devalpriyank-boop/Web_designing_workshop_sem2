74776e86c4b43ee299a8c556a0d7b5f2f3339e77
Priyank Deval <devalpriyank@gmail.com>
2026-05-18 14:37:37 +0530

    CLASS ASSESSMENT

---
+++ CLASS ASSESSMENT/ASSESSMENT 7/my-first-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import StudentCard from "./components/StudentCard";
+
+function App() {
+  return (
+    <div>
+      <Header title="📋 Student App" />
+
+      <StudentCard name="Aman" marks={92} />
+      <StudentCard name="Priya" marks={78} />
+      <StudentCard name="Rahul" marks={85} />
+    </div>
+  );
+}
+
+export default App;
